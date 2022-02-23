6e01b6bb22b47ab516209ec30fc3ca9f2093aa1e
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-23 18:20:20 +0100

    added a login router

--- src/server/routers/usersRouter.js
+++ src/server/routers/usersRouter.js
@@ -1,31 +1,7 @@
-const bcrypt = require("bcrypt");
-const jsonwebtoken = require("jsonwebtoken");
-const User = require("../../database/models/User");
+require("dotenv").config();
+const express = require("express");
+const login = require("../controllers/usersController");
 
-const login = async (req, res, next) => {
-  const { username, password } = req.body;
+const router = express.Router();
 
-  const user = await User.findOne({ username });
-
-  if (!user) {
-    const error = new Error("Username or password are wrong");
-    error.code = 404;
-    next(error);
-  } else {
-    const userData = {
-      username: user.username,
-      id: user.id,
-    };
-    const rightPassword = await bcrypt.compare(password, user.password);
-    if (!rightPassword) {
-      const error = new Error("Username or password are wrong");
-      error.code = 403;
-      next(error);
-    } else {
-      const token = jsonwebtoken.sign(userData, process.env.SECRET);
-      res.json({ token });
-    }
-  }
-};
-
-module.exports = login;
+router.post("/login", login);
