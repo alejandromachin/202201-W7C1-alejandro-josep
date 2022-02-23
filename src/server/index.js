283679e571afbadadbf632b67220437fb0f8c15d
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-23 17:27:16 +0100

    added middleware for errors

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+app.use(morgan("dev"));
+
+app.use(notFoundError);
+app.use(generalError);
 
 module.exports = app;
