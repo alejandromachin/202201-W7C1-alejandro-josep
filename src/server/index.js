7551f45d01e4ff4be2feaec2be90b58ee060c4cb
Josep Conejero <bermeu@gmail.com>
2022-02-23 17:52:20 +0100

    Added seriesRouter.js

--- src/server/index.js
+++ src/server/index.js
@@ -3,11 +3,14 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const { notFoundError, generalError } = require("./middlewares/errors");
+const getAllSeries = require("./controllers/seriesControllers");
 
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(helmet());
+
+app.use("/series", getAllSeries);
 
 app.use(notFoundError);
 app.use(generalError);
--- src/server/routers/seriesRouter.js
+++ src/server/routers/seriesRouter.js
@@ -1,1 +1,9 @@
 const express = require("express");
+
+const { getAllSeries } = require("../controllers/seriesControllers");
+
+const seriesRouter = express.Router();
+
+seriesRouter.get("/", getAllSeries);
+
+module.exports = seriesRouter;
