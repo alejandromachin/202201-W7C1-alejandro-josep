60208ced247375cd6b1376f141168d65ea62273d
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-23 17:18:31 +0100

    added port and connection string and app to the iffe

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const connectToDataBase = require("./database");
+const app = require("./server");
 const serverUp = require("./server/serverUp");
 
+const port = process.env.PORT || 4000;
+const connectionString = process.env.MONGODB;
+
 (async () => {
-  await serverUp();
-  await connectToDataBase();
+  await serverUp(port, app);
+  await connectToDataBase(connectionString);
 })();
