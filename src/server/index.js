44a0f463b50c0323726b0bf86aa63654d954ee06
Josep Conejero <bermeu@gmail.com>
2022-02-23 17:47:54 +0100

    Added getAllSeries in seriesControllers

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require("helmet");
 const { notFoundError, generalError } = require("./middlewares/errors");
 
 const app = express();
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(helmet());
 
 app.use(notFoundError);
 app.use(generalError);
