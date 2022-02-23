8b451730e4b483798ff044e03e1a5a6652f1a015
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-23 18:02:29 +0100

    added general error test

--- src/server/middlewares/errors.test.js
+++ src/server/middlewares/errors.test.js
@@ -1,4 +1,4 @@
-const { notFoundError } = require("./errors");
+const { notFoundError, generalError } = require("./errors");
 
 describe("Given a notFoundError middleware", () => {
   describe("When it receives a request", () => {
@@ -13,6 +13,24 @@ describe("Given a notFoundError middleware", () => {
 
       expect(res.json).toBeCalledWith(error);
       expect(res.status).toBeCalledWith(statusCode);
+    });
+  });
+});
+
+describe("Given a generalError middleware", () => {
+  describe("When it receives a request", () => {
+    test("Then it should call the response json method with the message 'all wrong' and status 500", async () => {
+      const err = { error: true, message: "General error" };
+      const errorStatus = 500;
+      const res = {
+        json: jest.fn(),
+        status: jest.fn(),
+      };
+
+      await generalError(err, null, res);
+
+      expect(res.json).toBeCalledWith(err);
+      expect(res.status).toBeCalledWith(errorStatus);
     });
   });
 });
