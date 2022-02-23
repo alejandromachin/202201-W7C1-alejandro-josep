28cdcd36287349af2ea9a8cfc49fb44b2fd7afb2
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-23 17:30:09 +0100

    exported errors to index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const { notFoundError, generalError } = require("./middlewares/errors");
 
 const app = express();
 app.use(morgan("dev"));
--- src/server/middlewares/errors.js
+++ src/server/middlewares/errors.js
@@ -2,4 +2,11 @@ const notFoundError = (req, res) => {
   res.status(404).json({ error: true, message: "Endpoint not found" });
 };
 
-module.exports = notFoundError;
+// eslint-disable-next-line no-unused-vars
+const generalError = (err, req, res, next) => {
+  const errorCode = err.code ?? 500;
+  const errorMessage = err.code ? err.message : "General error";
+  res.status(errorCode).json({ error: true, message: errorMessage });
+};
+
+module.exports = { notFoundError, generalError };
