50d6c4d1997293687ad7cdfa1017c4b56585c965
Josep Conejero <bermeu@gmail.com>
2022-02-23 18:52:13 +0100

    Added seriesControllers.test.js

---
+++ src/server/controllers/seriesControllers.test.js
@@ -0,0 +1,26 @@
+const Serie = require("../../database/models/Serie");
+const getAllSeries = require("./seriesControllers");
+
+jest.mock("../../database/models/Serie");
+
+describe("Given a getAllSeries controller", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+  describe("When it receives a response", () => {
+    test("Then it should call method json with a list of series in the response", async () => {
+      const res = { json: jest.fn() };
+
+      const series = {
+        id: 1,
+        name: "Death Note",
+        admin: true,
+      };
+      Serie.find = jest.fn().mockResolvedValue(series);
+
+      await getAllSeries(null, res);
+      expect(Serie.find).toHaveBeenCalled();
+      expect(res.json).toHaveBeenLastCalledWith({ series });
+    });
+  });
+});
--- src/server/index.js
+++ src/server/index.js
@@ -3,14 +3,14 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const { notFoundError, generalError } = require("./middlewares/errors");
-const getAllSeries = require("./controllers/seriesControllers");
+const seriesRouter = require("./routers/seriesRouter");
 
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(helmet());
 
-app.use("/series", getAllSeries);
+app.use("/series", seriesRouter);
 
 app.use(notFoundError);
 app.use(generalError);
