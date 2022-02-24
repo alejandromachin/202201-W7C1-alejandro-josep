2174cdb407c8aee50708e1aeb16896b99ba18164
Josep Conejero <bermeu@gmail.com>
2022-02-24 18:49:17 +0100

    Added postSeries to seriesControllers

--- src/server/routers/seriesRouter.js
+++ src/server/routers/seriesRouter.js
@@ -3,11 +3,17 @@ const express = require("express");
 const {
   getAllSeries,
   getViewedSeries,
+  postSerie,
 } = require("../controllers/seriesControllers");
 
 const seriesRouter = express.Router();
 
 seriesRouter.get("/", getAllSeries);
 seriesRouter.get("/viewed", getViewedSeries);
+// seriesRouter.get("/pending", getPendingSeries);
+seriesRouter.post("/", postSerie);
+// seriesRouter.put("/:idSerie", putSerie);
+// seriesRouter.delete("/:idSerie", deleteSerie);
+// seriesRouter.patch("/view/:idSerie", matchSerie);
 
 module.exports = seriesRouter;
